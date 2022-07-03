929da13951e877cdfb91613982fb599937b95b9a
AshleyCJ <61482642+AshleyCJ@users.noreply.github.com>
2022-07-03 12:38:51 +0200

    All pictures and videos added to MyWork page

--- Webpage/index.js
+++ Webpage/index.js
@@ -3,44 +3,36 @@ let VideoName = [
     "Corridor 4.mp4",
     "Corridor 7.mp4",
     "Corridor 8.mp4",
-    "Corridor 9.mp4"
+    "Corridor 9.mp4",
+    "Microbuild 3.mp4",
+    "Microbuild 1.mp4",
+    "Pronouns.mp4",
+    "First Day.mp4",
+    "Dual Mechanic 2.mp4",
+    "Dual Mechanic.mp4",
+    "African Puzzle.mp4",
+    "Angry balls.mp4",
+    "PongMod.mp4",
+    "Blender animation 1.mp4",
+    "Blender animation 2.mp4"
 ]
 
 function LoadVideo(ButtonNum)
     {
         var RequestID = ButtonNum.id;
         var VideoNum = parseInt(RequestID.slice(-2));
-        var InsertID = "InsertClip";
-        if (VideoNum < 10)
-        {
-            InsertID += "0" + VideoNum;
-        }
-        else
-        {
-            InsertID += VideoNum;
-        }
         var VideoPath = "MyWorkImages/Videos/" + VideoName[VideoNum - 1];
-
-
-        alert(VideoPath);
-
-
-
         var NewVideo = document.createElement('video');
         NewVideo.src = VideoPath;
         NewVideo.controls = true;
-        NewVideo.height = 500;
-        NewVideo.width = 1000;
-
-        var InsertLocation = document.getElementById(InsertID);
+        //NewVideo.height = 500;
+        //NewVideo.width = 1000;
+        var InsertID = "InsertedClip";
+        if (document.contains(document.getElementById(InsertID)))
+        {
+            document.getElementById(InsertID).remove();
+        }
+        NewVideo.setAttribute("id", InsertID);
+        var InsertLocation = ButtonNum.parentNode;
         InsertLocation.appendChild(NewVideo);
     }
-
-
-/*
-<video width="320" height="240" controls>
-  <source src="movie.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
-Your browser does not support the video tag.
-</video>
-*/
